c0c33015c69b71c53b1dcdb2e6a26a873e242201
hossein shirinegad <hosseinshirinegad.66@gmail.com>
2024-08-19 19:03:10 +0330

    worked on sidebar

--- src/components/layoutes/Sidebar.tsx
+++ src/components/layoutes/Sidebar.tsx
@@ -1,165 +1,173 @@
 import { NavLink } from "react-router-dom";
-import $ from "jquery";
 import { useEffect } from "react";
 export default function Sidebar() {
   useEffect(() => {
-    // Toggle Sidebar
-    $(document).on("click", '[data-bs-toggle="sidebar"]', function (event) {
-      event.preventDefault();
-      $(".app").toggleClass("sidenav-toggled");
-    });
-
-    $(".app-sidebar").hover(
-      function () {
-        if ($("body").hasClass("sidenav-toggled")) {
-          $("body").addClass("sidenav-toggled-open");
-        }
-      },
-      function () {
-        if ($("body").hasClass("sidenav-toggled")) {
-          $("body").removeClass("sidenav-toggled-open");
-        }
-      }
-    );
-
-    // Activate sidebar slide toggle
-    $("[data-bs-toggle='slide']").on("click", function (e) {
-      var $this = $(this);
-      var checkElement = $this.next();
-      var animationSpeed = 300,
-        slideMenuSelector = ".slide-menu";
-      if (checkElement.is(slideMenuSelector) && checkElement.is(":visible")) {
-        checkElement.slideUp(animationSpeed, function () {
-          checkElement.removeClass("open");
-        });
-        checkElement.parent("li").removeClass("is-expanded");
-      } else if (
-        checkElement.is(slideMenuSelector) &&
-        !checkElement.is(":visible")
-      ) {
-        var parent = $this.parents("ul").first();
-        var ul = parent.find("ul:visible").slideUp(animationSpeed);
-        ul.removeClass("open");
-        var parent_li = $this.parent("li");
-        checkElement.slideDown(animationSpeed, function () {
-          checkElement.addClass("open");
-          parent.find("li.is-expanded").removeClass("is-expanded");
-          parent_li.addClass("is-expanded");
-        });
-      }
-      if (checkElement.is(slideMenuSelector)) {
-        e.preventDefault();
-      }
-    });
-
-    // Activate sidebar slide toggle
-    $("[data-bs-toggle='sub-slide']").on("click", function (e) {
-      var $this = $(this);
-      var checkElement = $this.next();
-      var animationSpeed = 300,
-        slideMenuSelector = ".sub-slide-menu";
-      if (checkElement.is(slideMenuSelector) && checkElement.is(":visible")) {
-        checkElement.slideUp(animationSpeed, function () {
-          checkElement.removeClass("open");
-        });
-        checkElement.parent("li").removeClass("is-expanded");
-      } else if (
-        checkElement.is(slideMenuSelector) &&
-        !checkElement.is(":visible")
-      ) {
-        var parent = $this.parents("ul").first();
-        var ul = parent.find("ul:visible").slideUp(animationSpeed);
-        ul.removeClass("open");
-        var parent_li = $this.parent("li");
-        checkElement.slideDown(animationSpeed, function () {
-          checkElement.addClass("open");
-          parent.find("li.is-expanded").removeClass("is-expanded");
-          parent_li.addClass("is-expanded");
-        });
-      }
-      if (checkElement.is(slideMenuSelector)) {
-        e.preventDefault();
-      }
-    });
-
-    // Activate sidebar slide toggle
-    $("[data-bs-toggle='sub-slide-sub']").on("click", function (e) {
-      var $this = $(this);
-      var checkElement = $this.next();
-      var animationSpeed = 300,
-        slideMenuSelector = ".sub-slide-menu-sub";
-      if (checkElement.is(slideMenuSelector) && checkElement.is(":visible")) {
-        checkElement.slideUp(animationSpeed, function () {
-          checkElement.removeClass("open");
-        });
-        // checkElement.parent("li").removeClass("is-expanded");
-      } else if (
-        checkElement.is(slideMenuSelector) &&
-        !checkElement.is(":visible")
-      ) {
-        var parent = $this.parents("ul").first();
-        var ul = parent.find("ul:visible").slideUp(animationSpeed);
-        ul.removeClass("open");
-        var parent_li = $this.parent("li");
-        checkElement.slideDown(animationSpeed, function () {
-          checkElement.addClass("open");
-          parent.find("li.is-expanded").removeClass("is-expanded");
-          parent_li.addClass("is-expanded");
-        });
-      }
-      if (checkElement.is(slideMenuSelector)) {
-        e.preventDefault();
-      }
-    });
-
-    // ______________Active Class
-    $(".app-sidebar li a").each(function () {
-      var pageUrl = window.location.href.split(/[?#]/)[0];
-      if (this.getAttribute("href")) {
-        if (this?.href == pageUrl) {
-          $(this).addClass("active");
-          $(this).parent().addClass("is-expanded");
-          $(this).parent().parent().prev().addClass("active");
-          $(this).parent().parent().addClass("open");
-          $(this).parent().parent().prev().addClass("is-expanded");
-          $(this).parent().parent().parent().addClass("is-expanded");
-          $(this).parent().parent().parent().parent().addClass("open");
-          $(this).parent().parent().parent().parent().prev().addClass("active");
-          $(this)
-            .parent()
-            .parent()
-            .parent()
-            .parent()
-            .parent()
-            .addClass("is-expanded");
-        }
-      }
-    });
-
-    var toggleSidebar = function () {
-      var w: any = $(window);
-      if (w.outerWidth() <= 767) {
-        $("body").addClass("sidebar-gone");
-        $(document)
-          .off("click", "body")
-          .on("click", "body", function (e) {
-            if (
-              $(e.target).hasClass("sidebar-show") ||
-              $(e.target).hasClass("search-show")
-            ) {
-              $("body").removeClass("sidebar-show");
-              $("body").addClass("sidebar-gone");
-              $("body").removeClass("search-show");
-            }
-          });
-      } else {
-        $("body").removeClass("sidebar-gone");
-      }
-    };
-    
-    toggleSidebar();
-    $(window).resize(toggleSidebar);
+    // // Toggle Sidebar
+    // $(document).on("click", '[data-bs-toggle="sidebar"]', function (event) {
+    //   event.preventDefault();
+    //   $(".app").toggleClass("sidenav-toggled");
+    // });
+    // $(".app-sidebar").hover(
+    //   function () {
+    //     if ($("body").hasClass("sidenav-toggled")) {
+    //       $("body").addClass("sidenav-toggled-open");
+    //     }
+    //   },
+    //   function () {
+    //     if ($("body").hasClass("sidenav-toggled")) {
+    //       $("body").removeClass("sidenav-toggled-open");
+    //     }
+    //   }
+    // );
+    // // Activate sidebar slide toggle
+    // $("[data-bs-toggle='slide']").on("click", function (e) {
+    //   var $this = $(this);
+    //   var checkElement = $this.next();
+    //   var animationSpeed = 300,
+    //     slideMenuSelector = ".slide-menu";
+    //   if (checkElement.is(slideMenuSelector) && checkElement.is(":visible")) {
+    //     checkElement.slideUp(animationSpeed, function () {
+    //       checkElement.removeClass("open");
+    //     });
+    //     checkElement.parent("li").removeClass("is-expanded");
+    //   } else if (
+    //     checkElement.is(slideMenuSelector) &&
+    //     !checkElement.is(":visible")
+    //   ) {
+    //     var parent = $this.parents("ul").first();
+    //     var ul = parent.find("ul:visible").slideUp(animationSpeed);
+    //     ul.removeClass("open");
+    //     var parent_li = $this.parent("li");
+    //     checkElement.slideDown(animationSpeed, function () {
+    //       checkElement.addClass("open");
+    //       parent.find("li.is-expanded").removeClass("is-expanded");
+    //       parent_li.addClass("is-expanded");
+    //     });
+    //   }
+    //   if (checkElement.is(slideMenuSelector)) {
+    //     e.preventDefault();
+    //   }
+    // });
+    // // Activate sidebar slide toggle
+    // $("[data-bs-toggle='sub-slide']").on("click", function (e) {
+    //   var $this = $(this);
+    //   var checkElement = $this.next();
+    //   var animationSpeed = 300,
+    //     slideMenuSelector = ".sub-slide-menu";
+    //   if (checkElement.is(slideMenuSelector) && checkElement.is(":visible")) {
+    //     checkElement.slideUp(animationSpeed, function () {
+    //       checkElement.removeClass("open");
+    //     });
+    //     checkElement.parent("li").removeClass("is-expanded");
+    //   } else if (
+    //     checkElement.is(slideMenuSelector) &&
+    //     !checkElement.is(":visible")
+    //   ) {
+    //     var parent = $this.parents("ul").first();
+    //     var ul = parent.find("ul:visible").slideUp(animationSpeed);
+    //     ul.removeClass("open");
+    //     var parent_li = $this.parent("li");
+    //     checkElement.slideDown(animationSpeed, function () {
+    //       checkElement.addClass("open");
+    //       parent.find("li.is-expanded").removeClass("is-expanded");
+    //       parent_li.addClass("is-expanded");
+    //     });
+    //   }
+    //   if (checkElement.is(slideMenuSelector)) {
+    //     e.preventDefault();
+    //   }
+    // });
+    // // Activate sidebar slide toggle
+    // $("[data-bs-toggle='sub-slide-sub']").on("click", function (e) {
+    //   var $this = $(this);
+    //   var checkElement = $this.next();
+    //   var animationSpeed = 300,
+    //     slideMenuSelector = ".sub-slide-menu-sub";
+    //   if (checkElement.is(slideMenuSelector) && checkElement.is(":visible")) {
+    //     checkElement.slideUp(animationSpeed, function () {
+    //       checkElement.removeClass("open");
+    //     });
+    //     // checkElement.parent("li").removeClass("is-expanded");
+    //   } else if (
+    //     checkElement.is(slideMenuSelector) &&
+    //     !checkElement.is(":visible")
+    //   ) {
+    //     var parent = $this.parents("ul").first();
+    //     var ul = parent.find("ul:visible").slideUp(animationSpeed);
+    //     ul.removeClass("open");
+    //     var parent_li = $this.parent("li");
+    //     checkElement.slideDown(animationSpeed, function () {
+    //       checkElement.addClass("open");
+    //       parent.find("li.is-expanded").removeClass("is-expanded");
+    //       parent_li.addClass("is-expanded");
+    //     });
+    //   }
+    //   if (checkElement.is(slideMenuSelector)) {
+    //     e.preventDefault();
+    //   }
+    // });
+    // // ______________Active Class
+    // $(".app-sidebar li a").each(function () {
+    //   var pageUrl = window.location.href.split(/[?#]/)[0];
+    //   if (this.getAttribute("href")) {
+    //     if (this?.href == pageUrl) {
+    //       $(this).addClass("active");
+    //       $(this).parent().addClass("is-expanded");
+    //       $(this).parent().parent().prev().addClass("active");
+    //       $(this).parent().parent().addClass("open");
+    //       $(this).parent().parent().prev().addClass("is-expanded");
+    //       $(this).parent().parent().parent().addClass("is-expanded");
+    //       $(this).parent().parent().parent().parent().addClass("open");
+    //       $(this).parent().parent().parent().parent().prev().addClass("active");
+    //       $(this)
+    //         .parent()
+    //         .parent()
+    //         .parent()
+    //         .parent()
+    //         .parent()
+    //         .addClass("is-expanded");
+    //     }
+    //   }
+    // });
+    // var toggleSidebar = function () {
+    //   var w: any = $(window);
+    //   if (w.outerWidth() <= 767) {
+    //     $("body").addClass("sidebar-gone");
+    //     $(document)
+    //       .off("click", "body")
+    //       .on("click", "body", function (e) {
+    //         if (
+    //           $(e.target).hasClass("sidebar-show") ||
+    //           $(e.target).hasClass("search-show")
+    //         ) {
+    //           $("body").removeClass("sidebar-show");
+    //           $("body").addClass("sidebar-gone");
+    //           $("body").removeClass("search-show");
+    //         }
+    //       });
+    //   } else {
+    //     $("body").removeClass("sidebar-gone");
+    //   }
+    // };
+    // toggleSidebar();
+    // $(window).resize(toggleSidebar);
   }, []);
+
+  function menu(e: React.MouseEvent<HTMLLIElement, MouseEvent>) {
+    e.currentTarget.classList.add("active");
+    let list: any = document.querySelectorAll(".slide");
+    list.forEach((el: any) => {
+      if (!el.classList.contains("active")) {
+        el.classList.remove("is-expanded");
+      }
+    });
+    if (e.currentTarget.classList.contains("is-expanded")) {
+      e.currentTarget.classList.remove("is-expanded");
+    } else {
+      e.currentTarget.classList.add("is-expanded");
+    }
+    e.currentTarget.classList.remove("active");
+  }
 
   return (
     <>
@@ -240,50 +248,7 @@ export default function Sidebar() {
                 </span>
               </NavLink>
             </li>
-            <li className="slide">
-              <NavLink className="side-menu__item" to={"/test"}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="side-menu__icon"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M0 0h24v24H0V0z" fill="none" />
-                  <path
-                    d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z"
-                    opacity=".3"
-                  />
-                  <path d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
-                </svg>
-                <span className="side-menu__label">Test</span>
-                <span className="badge bg-success text-light" id="bg-side-text">
-                  1
-                </span>
-              </NavLink>
-            </li>
-            <li className="side-item side-item-category">عمومی</li>
-            <li className="slide">
-              <a className="side-menu__item" href="icons.html">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="side-menu__icon"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M0 0h24v24H0V0z" fill="none" />
-                  <path
-                    d="M12 4c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm3.5 4c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zm-7 0c.83 0 1.5.67 1.5 1.5S9.33 11 8.5 11 7 10.33 7 9.5 7.67 8 8.5 8zm3.5 9.5c-2.33 0-4.32-1.45-5.12-3.5h1.67c.7 1.19 1.97 2 3.45 2s2.76-.81 3.45-2h1.67c-.8 2.05-2.79 3.5-5.12 3.5z"
-                    opacity=".3"
-                  />
-                  <circle cx="15.5" cy="9.5" r="1.5" />
-                  <circle cx="8.5" cy="9.5" r="1.5" />
-                  <path d="M12 16c-1.48 0-2.75-.81-3.45-2H6.88c.8 2.05 2.79 3.5 5.12 3.5s4.32-1.45 5.12-3.5h-1.67c-.69 1.19-1.97 2-3.45 2zm-.01-14C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" />
-                </svg>
-                <span className="side-menu__label">آیکون ها</span>
-                <span className="badge bg-danger text-light" id="bg-side-text">
-                  جدید
-                </span>
-              </a>
-            </li>
-            <li className="slide">
+            <li className="slide" onClick={(e) => menu(e)}>
               <a className="side-menu__item" data-bs-toggle="slide" href="#">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -333,7 +298,7 @@ export default function Sidebar() {
                 </li>
               </ul>
             </li>
-            <li className="slide">
+            {/* <li className="slide">
               <a className="side-menu__item" data-bs-toggle="slide" href="#">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -1070,7 +1035,7 @@ export default function Sidebar() {
                   </ul>
                 </li>
               </ul>
-            </li>
+            </li> */}
           </ul>
         </div>
       </aside>
