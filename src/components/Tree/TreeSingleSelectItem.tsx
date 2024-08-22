7777bce8040b3692534206cbf260df194bb47336
hossein shirinegad <hosseinshirinegad.66@gmail.com>
2024-08-22 11:45:37 +0330

    created tree

--- src/components/Tree/TreeSingleSelectItem.tsx
+++ src/components/Tree/TreeSingleSelectItem.tsx
@@ -6,50 +6,77 @@ export default function TreeSingleSelectItem({
   trees: [];
   tree_name: string;
 }) {
-  function checkboxClick(item: any) {
-    $("." + tree_name).prop("checked", false);
-    $("#" + item.id + tree_name).prop("checked", true);
+  function open(item: any) {
+    $(".checkbox_" + tree_name).prop("checked", false);
+    $("#sub-tree-ul-" + tree_name + "-" + item.id).toggleClass("d-none");
+    if ($("#icon-" + tree_name + "_" + item.id).hasClass("si-plus")) {
+      $("#icon-" + tree_name + "_" + item.id).removeClass("si-plus");
+      $("#icon-" + tree_name + "_" + item.id).addClass("si-minus ");
+    } else {
+      $("#icon-" + tree_name + "_" + item.id).addClass("si-plus");
+      $("#icon-" + tree_name + "_" + item.id).removeClass("si-minus ");
+    }
+    console.log(item);
+    // useLocalStorageService.setTreeSelectedItem(props.tree_name, item);
+  }
+  function single(item: any) {
+    $(".checkbox_" + tree_name).prop("checked", false);
+
+    console.log(item);
     // useLocalStorageService.setTreeSelectedItem(props.tree_name, item);
   }
 
-  return trees.map((item: any, i: number) => {
-    return (
-      <li
-        className={`tree-item ${item.children.length ? "branch" : ""}`}
-        key={i}
-      >
-        <div
-          className="d-flex align-items-center"
-          style={{ whiteSpace: "nowrap" }}
-        >
-          <div className="checkbox d-flex">
-            <div
-              className={`custom-checkbox custom-control ${
-                item.children.length == 0 ? "leaf" : ""
-              }`}
-            >
-              <input
-                type="checkbox"
-                onChange={() => checkboxClick(item)}
-                className={`custom-control-input ${tree_name}`}
-                id={item.id.toString() + tree_name}
-              />
-              <label
-                htmlFor={item.id.toString() + tree_name}
-                className="custom-control-label mt-1"
-              >
-                <span className="pr-4"></span>
-              </label>
-            </div>
-          </div>
-          {item.persianTitle}
+  return trees.map((item: any) => (
+    <li key={item.id} style={{ listStyle: "none", marginBottom: 10 }}>
+      {item.children.length > 0 && (
+        <div className="d-flex align-items-center">
+          <input
+            className={"checkbox_" + tree_name}
+            value={item.id}
+            type="checkbox"
+            id={"input-checkbox-" + tree_name + "_" + item.id}
+          />
+          <i
+            id={"icon-" + tree_name + "_" + item.id}
+            className="si si-plus me-1"
+          ></i>
+          <label
+            style={{ cursor: "pointer", marginBottom: 0 }}
+            onClick={() => open(item)}
+            htmlFor={"input-checkbox-" + tree_name + "_" + item.id}
+            className="ms-2"
+          >
+            {item.persianTitle}
+          </label>
         </div>
-        {item.children.length && (
-          <ul>
-            <TreeSingleSelectItem tree_name={tree_name} trees={item.children} />
-          </ul>
-        )}
-      </li>
-    );
-  });
+      )}
+      {item.children.length === 0 && (
+        <div className="d-flex align-items-center">
+          <input
+            id={"input-checkbox-" + tree_name + "_" + item.id}
+            type="checkbox"
+            value={item.id}
+            className={"checkbox_" + tree_name}
+          />
+          <label
+            onClick={() => single(item)}
+            style={{ cursor: "pointer", marginBottom: 0 }}
+            htmlFor={"input-checkbox-" + tree_name + "_" + item.id}
+            className="ms-2"
+          >
+            {item.persianTitle}
+          </label>
+        </div>
+      )}
+
+      {item.children.length > 0 && (
+        <ul
+          className="d-none sub-tree-ul"
+          id={"sub-tree-ul-" + tree_name + "-" + item.id}
+        >
+          <TreeSingleSelectItem tree_name={tree_name} trees={item.children} />
+        </ul>
+      )}
+    </li>
+  ));
 }
