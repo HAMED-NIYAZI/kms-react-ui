0d076c6e39e84f57316eb0057517d11bbe932c52
Hamed.Niazmand <hamed.niyazi5@gmail.com>
2024-08-22 12:47:19 +0330

    .

--- src/components/layoutes/Master.tsx
+++ src/components/layoutes/Master.tsx
@@ -3,14 +3,13 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 
-export default function Master(/*props: { children: React.ReactNode }*/) {
+export default function Master() {
   return (
     <>
       <Sidebar />
       <div className="main-content app-content">
         <Header />
         <div className="container-fluid" style={{ marginTop: "90px" }}>
-          {/* {props.children} */}
           <Outlet />
         </div>
       </div>
