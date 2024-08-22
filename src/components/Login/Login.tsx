51e83c320d573816533819671f838da681404fa0
Hamed.Niazmand <hamed.niyazi5@gmail.com>
2024-08-22 09:28:14 +0330

    remove toastcontainer

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -265,18 +265,6 @@ function Login({
           </div>
         </div>
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
     </div>
   );
 }
