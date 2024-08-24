5eb77708a02ba10651030c2f6fbbc96f13cae06e
Hamed <USER@DESKTOP-20DRNS1>
2024-08-25 01:11:22 +0330

    refactor shapes

--- src/components/KnowledgeField/SingleSelectTreeComponent.tsx
+++ src/components/KnowledgeField/SingleSelectTreeComponent.tsx
@@ -24,35 +24,37 @@ function SingleSelectTreeComponent({
       <div className="card">
         <div className="card-header pb-0" style={{paddingBottom:'15px'}}>
           <div className="d-flex justify-content-between">
-            <h4
-              className="card-title mg-b-0"
-              style={{ paddingTop: "10px!important" }}
-            >
-              {tree_caption}
-            </h4>
-            <p>{treeSingleSelectValue[tree_name]?.persianTitle}</p>
-            <div className="d-flex gap-1"></div>
-           { (treeSingleSelectValue[tree_name]?.persianTitle && onDelete )&& (
-            <a
-              href="#"
-              onClick={() => {
-                if (onDelete) {
-                  onDelete(
-                    treeSingleSelectValue[tree_name].id,
-                    treeSingleSelectValue[tree_name].persianTitle
-                  );
-                  setTreeItem(tree_name, "");
-                }
-              }}
-              title="حذف "
-            >
-              <i className="fa fa-trash text-danger"></i>
-            </a>
+          <h4 className="card-title mg-b-0" style={{ paddingTop: "10px!important" }}>
+    {tree_caption}
+    {treeSingleSelectValue[tree_name]?.persianTitle && (
+      <span className="badge badge-warning mx-3" style={{textTransform:'none',fontSize:'14px'}}>{treeSingleSelectValue[tree_name]?.persianTitle}</span>
+    )}
+  </h4>
+             <div className="d-flex gap-1"> { (treeSingleSelectValue[tree_name]?.persianTitle && onDelete )&& (
+            
+            
+            <button type="button" className="btn btn-danger btn-icon" id="m-l-c-05"
+            onClick={() => {
+              if (onDelete) {
+                onDelete(
+                  treeSingleSelectValue[tree_name].id,
+                  treeSingleSelectValue[tree_name].persianTitle
+                );
+                setTreeItem(tree_name, "");
+              }
+            }}
+            title="حذف "><i className="fa fa-trash"></i></button>
+   
+      
             )}
-                          <a href="#" onClick={() => {onReload();
+ 
+
+              <button type="button" className="btn btn-success btn-icon" onClick={() => {onReload();
                            setTreeItem(tree_name, "");}} title="بروزرسانی">
-                بروزرسانی
-              </a>
+                <i className="mdi mdi-refresh"></i>
+              </button></div>
+          
+
           </div>
         </div>
         <div className="card-body">
