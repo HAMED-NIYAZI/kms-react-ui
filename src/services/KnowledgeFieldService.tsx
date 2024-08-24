b8295076394553c02ba5d0c243d8b6c32e33d8d6
Hamed <USER@DESKTOP-20DRNS1>
2024-08-24 21:12:38 +0330

    addmknowledgefieldservice.tsx

--- src/services/KnowledgeFieldService.tsx
+++ src/services/KnowledgeFieldService.tsx
@@ -1,20 +1,21 @@
-import api from '@/axios/api'
+import api from "../api/api";
 class KnowledgeFieldService {
     async getKnowledgeFieldTree() {
         return await api.get(`/api/KnowledgeField/GetKnowledgeFieldTree`);
     }
-    async create(data) {
+    async create(data: any) {
         return await api.post(`api/KnowledgeField/Add`, data);
     }
-    async getById(id) {
+    async getById(id:string) {
         return await api.get(`api/KnowledgeField/GetById/${id}`);
     }
-    async update(data) {
+    async update(data: any) {
         return await api.put(`api/KnowledgeField/update`, data);
     }
-    async delete(id) {
+    async delete(id:string) {
         return await api.delete(`api/KnowledgeField/DeleteById/${id}`);
     }
 }
 
-export default new KnowledgeFieldService
+export default new KnowledgeFieldService();
+
